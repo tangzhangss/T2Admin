d35a2138fdc4e89d11ebbad230259b554e7158ae
tangzhang <zhang.tang@cy-tech.net>
2022-04-18 16:05:09 +0800

    新增后端接口权限；优化工具类runtimeExecutor

---
+++ api/common-service/src/main/java/com/tangzhangss/commonservice/aspect/syslog/SysLogApi.java
@@ -0,0 +1,11 @@
+package com.tangzhangss.commonservice.aspect.syslog;
+
+
+import com.tangzhangss.commonutils.base.SysBaseApi;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sys_log")
+public class SysLogApi extends SysBaseApi<SysLogEntity, SysLogService> {
+}
---
+++ api/common-service/src/main/java/com/tangzhangss/commonservice/aspect/syslog/SysLogDao.java
@@ -0,0 +1,5 @@
+package com.tangzhangss.commonservice.aspect.syslog;
+import com.tangzhangss.commonutils.base.SysBaseDao;
+
+public interface SysLogDao extends SysBaseDao<SysLogEntity, String> {
+}
